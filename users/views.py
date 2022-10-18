300f410599847d54246b4e9b87b971efc6e3e902
Veidz <joaovictorveidz@gmail.com>
2022-10-18 16:24:28 -0300

    feat: create create view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import User
 
 def home(request):
   users = User.objects.all()
   return render(request, 'index.html', {'users': users})
+
+def create(request):
+  name = request.POST.get('name')
+  User.objects.create(name=name)
+  return redirect(home)
